c7204b116ccab81dd7c90d82f21e008a04c49c85
kaloshina <65906907+kaloshina@users.noreply.github.com>
2025-09-23 11:58:33 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,36 +8,42 @@ pygame.display.set_caption("Black Hole Sim")
 
 WINDOW.fill((0, 0, 0))
 
+
+# simulation is run in the equatorial plane, where the vertical angle theta = pi/2
+method = 1 # method 1 is geodesic eq, method 2 uses conserved quantities and performs first integrals
+
 # black hole properties (where G and c are 1)
-M = 50
-J = 0 
+M = 120.     # mass
+J = 0.5*M**2.      # angular momentum
 a = J/M
-r_s = 2*M
-r_p = r_s/2 + np.sqrt((r_s/2)**2 - a**2) # outer event horizon
+r_p = M + np.sqrt(M**2 - a**2) # outer event horizon
 
-BHPos = np.array([960, 540])
+# setting up the black hole at the center of the screen
+BHPos = np.array([960., 540.])
 
-v = 5000.  # speed of paths being traced out (not necessarily c)
-
+v = 1.  # speed of paths being traced out (not necessarily c)
 
 
 class blackHole:
     def __init__(self, xpos, ypos):
+        '''Black Hole only has a position attribute'''
         self.pos = np.array([xpos, ypos])
         self.draw()
     
     def draw(self):
-        pygame.draw.circle(WINDOW, (255, 0, 0), self.pos, r_s)  # Draw a red circle for the black hole
-        pygame.draw.circle(WINDOW, (0, 0, 0), self.pos, r_s-2)
+        pygame.draw.circle(WINDOW, (255, 0, 0), self.pos, r_p)  # Draw a red circle for the black hole
+        pygame.draw.circle(WINDOW, (0, 0, 0), self.pos, r_p-3)
 
 
 class lightRay:
     def __init__(self, xpos, ypos, velAngle):
-        # cartesian position and velocity
+        '''Light Ray object'''
+        # cartesian positions (absolute and relative to black hole)
         pos = np.array([xpos, ypos])
         self.posRel = pos - BHPos
+        # velocity vector
         vel = [v * np.cos(velAngle), -v * np.sin(velAngle)]
-        # polar position and velocity
+        # polar coordinates
         self.r = np.sqrt(self.posRel[0]**2 + self.posRel[1]**2)
         self.phi = 0
         self.update_phi()
@@ -45,19 +51,14 @@ class lightRay:
         self.dr = (self.posRel[0]*vel[0]+self.posRel[1]*vel[1])/self.r
         self.dphi = (self.posRel[0]*vel[1]-self.posRel[1]*vel[0])/self.r**2
 
-        # Kerr metric variables   
-        self.Sigma = self.r**2
-        self.Delta = self.r**2 - r_s*self.r + a**2
-
         # conserved quantity E
-        if a == 0:
-            self.E = np.sqrt(self.dr**2 + (self.r**2-self.r*r_s)*self.dphi**2)
-        else:
-            A = r_s*self.r/self.Sigma-1
-            B = -2*r_s*self.r*a*self.dphi/self.Sigma
-            C = self.Sigma/self.Delta*self.dr**2+(self.r**2+a**2+r_s*self.r*a**2/self.Sigma)*self.dphi**2
-            D = (-B+np.sqrt(B**2-4*A*C))/(2*A)
-            self.E = D*(r_s-self.r)/(r_s*a*self.dphi)
+        Delta = self.r*(self.r - 2.*M) + a**2
+        r = self.r
+        A = 2.*M/r-1
+        B = -4.*M*a*self.dphi/r
+        C = r**2*self.dr**2/Delta + (r**2 + a**2 + 2.*M*a**2/r)*self.dphi**2
+        dt = (-B + np.sqrt(B**2-4.*A*C))/(2.*A)
+        self.E = (1-2.*M/self.r)*dt + 2.*M*a*self.dphi/self.r
 
         self.alive = (self.r >= r_p)
         if self.alive:
@@ -65,48 +66,50 @@ class lightRay:
 
     def update(self, dl):
         if self.alive:
-
-            # conduct RK4 on r', phi', r and phi
-            k1 = self.d2r(self.dr)
-            k2 = self.d2r(self.dr+dl*k1/2)
-            k3 = self.d2r(self.dr+dl*k2/2)
-            k4 = self.d2r(self.dr+dl*k3)
-            self.dr += dl/6*(k1+2*k2+2*k3+k4)
-            k1 = self.d2phi(self.dphi)
-            k2 = self.d2phi(self.dphi+dl*k1/2)
-            k3 = self.d2phi(self.dphi+dl*k2/2)
-            k4 = self.d2phi(self.dphi+dl*k3)
-            self.dphi += dl/6*(k1+2*k2+2*k3+k4)
-            # update r and phi positions
-            self.r += self.dr*dl
-            self.phi += self.dphi*dl
+            # conduct RK4 on r' and phi'
+            if method == 1:
+                k1 = self.d2r(self.dr)
+                k2 = self.d2r(self.dr+dl*k1/2)
+                k3 = self.d2r(self.dr+dl*k2/2)
+                k4 = self.d2r(self.dr+dl*k3)
+                self.dr += dl/6*(k1+2*k2+2*k3+k4)
+                k1 = self.d2phi(self.dphi)
+                k2 = self.d2phi(self.dphi+dl*k1/2)
+                k3 = self.d2phi(self.dphi+dl*k2/2)
+                k4 = self.d2phi(self.dphi+dl*k3)
+                self.dphi += dl/6*(k1+2*k2+2*k3+k4)
+                # update r and phi positions
+                self.r += self.dr*dl
+                self.phi += self.dphi*dl
+            elif method == 2:
+                pass
             # convert position to cartesian and update location
             self.posRel = np.array([self.r*np.cos(self.phi), self.r*np.sin(self.phi)])
             # check if the light has crossed the event horizon
-            self.alive = (self.r >= r_p)
+            self.alive = (self.r > r_p+1)
             if self.alive:
                 self.draw()
 
     def d2r(self, dr):
-        if a == 0: return (r_s*(r_s-self.r)/(2.*self.r**3))*(self.E/(1.-r_s/self.r))**2 \
-                                - r_s/(2.*self.r*(self.r-r_s))*dr**2 \
-                                + (self.r-r_s)*self.dphi**2
-        else:
-            dt = (self.E*self.r - r_s*a*self.dphi)/(self.r-r_s)
-            return M/self.r**2*(1-2*M/self.r+a**2/self.r**2)*dt**2 \
-                    - 2*M*a/self.r**2*dt*self.dphi \
-                    - (self.r**2-self.r*M-M)/(self.r*self.Delta)*dr**2 \
-                    + (self.r-2*M+a**2/self.r)*self.dphi**2
-    
+        # radial acceleration
+        '''if a == 0: return -M*(self.r-2.*M)/(self.r**3)*(self.E/(1.-2.*M/self.r))**2 \
+                                + M/(self.r*(self.r-2.*M))*dr**2 \
+                                + (self.r-2.*M)*self.dphi**2'''
+        Delta = self.r*(self.r - 2.*M) + a**2
+        dt = (self.E*self.r - 2.*M*a*self.dphi)/(self.r-2.*M)
+        return -M*Delta/self.r**4.*dt**2 \
+                + 2.*M*a*Delta/self.r**4*dt*self.dphi \
+                - (a**2-M*self.r)/(self.r*Delta)*dr**2 \
+                + Delta*(self.r**3-M*a**2)/self.r**4*self.dphi**2
+        
+        
     def d2phi(self, dphi):
-        if a == 0: return -2.*self.dr*dphi/self.r
-        else:
-            dt = (self.E*self.r - r_s*a*self.dphi)/(self.r-r_s)
-            return  -2*M*(self.r**2-a**2)/(self.r**2*self.Delta)*dt*self.dr \
-                    + 2*M/self.r**2*dt*dphi \
-                    - a/(self.r*self.Delta)*self.dr**2 \
-                    + M/self.r**2*dphi**2
-
+        # angular acceleration (derived using geodesic equation)
+        '''if a == 0: return -2.*self.dr*dphi/self.r'''
+        Delta = self.r*(self.r - 2.*M) + a**2
+        dt = (self.E*self.r - 2.*M*a*self.dphi)/(self.r-2.*M)
+        return -2.*self.dr/(self.r**2*Delta)*(M*a*dt + (self.r**3-2.*M*self.r**2-M*a**2)*self.dphi)
+    
     def update_phi(self):
         # properly sets up phi due to the range limitations of arctan
         if (self.posRel[0] != 0):
@@ -123,7 +126,7 @@ class lightRay:
             else: self.phi = -np.pi/2        
     
     def draw(self):
-        n=0.2
+        n=0.1
         pygame.draw.circle(WINDOW, self.wavelength_to_rgb(580/np.sqrt(1-n*r_s/self.r)), self.posRel+BHPos, 1)
 
     def wavelength_to_rgb(self, wavelength):
@@ -142,24 +145,22 @@ class lightRay:
 BH1 = blackHole(BHPos[0], BHPos[1])
 
 # setting up the light rays
-n = 108
+n = 216
 LRList = [None]*n
 for i in range(n):
-    LRList[i] = lightRay(0, -500+i*10, -0.7)
-    
-
-
+    #LRList[i] = lightRay(100, i*10, 0)
+    LRList[i] = lightRay(0, -540.+i*10., 0)
+#LRList[n-1] = lightRay(BHPos[0], BHPos[1]-3.*M, 0)
 
 # running each event sequentially
 while True:
     # update each light ray
     for LR in LRList:
-        LR.update(0.0001)
+        LR.update(1)
     # check if window is closed  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-
 
